ded616fa03e05d4a69938c34dad529b0811ad197
lukehungngo <qhung.bm0409@gmail.com>
2018-05-23 09:47:53 +1000

    Add story when buyProduct

--- contracts/UniCraft.sol
+++ contracts/UniCraft.sol
@@ -17,7 +17,7 @@ contract UniCraft is Ownable{
         bytes compellingStoryUri;
         bytes userStory;
         address owner;
-        uint8 status;
+        //uint8 status;
         //0: Available, 1: Unverified, 2: Verified
     }
     //mapping productID -> product
@@ -25,9 +25,9 @@ contract UniCraft is Ownable{
     
     event IssuerCertificate(bytes32 _id, bytes _name, address _addr, bytes _ssn);
     event CreateProduct(bytes32 _id, bytes32 _artisanId, uint256 _producedDate, bytes _cStory);
-    event BuyProduct(bytes32 _id, address buyer, uint256 _soldDate);
-    event AddUserStory(bytes32 _id, bytes _userStory);
-    event ConfirmProduct(bytes32 _id, bool _result);
+    event BuyProduct(bytes32 _id, address buyer, uint256 _soldDate, bytes _uStory);
+    // event AddUserStory(bytes32 _id, bytes _userStory);
+    // event ConfirmProduct(bytes32 _id, bool _result);
     function issuerCertificate(bytes32 _id, bytes _name, address _addr, bytes _ssn)
     public
     onlyOwner
@@ -53,41 +53,42 @@ contract UniCraft is Ownable{
         product.artisanId = _artisanId;
         product.producedDate = now;
         product.compellingStoryUri = _cStory;
-        product.status = 0;
+        //product.status = 0;
         products[_id] = product;
         artisans[_artisanId].productIds.push(_id);
         emit CreateProduct(_id, _artisanId, now, _cStory);
         return true;
     }
-    function buyProduct(bytes32 _id, uint256 _soldDate)
+    function buyProduct(bytes32 _id, bytes _userStory)
     public
     returns (bool)
     {
         require(products[_id].owner == address(0x0));
+        //products[_id].status == 1
         products[_id].owner = msg.sender;
-        products[_id].status = 1;
         products[_id].soldDate = now;
-        emit BuyProduct(_id, msg.sender, now);
-        return true;
-    }
-    function addUserStory(bytes32 _id, bytes _userStory)
-    public
-    returns (bool)
-    {
-        require(products[_id].owner == msg.sender);
-        require(products[_id].status == 1);
         products[_id].userStory = _userStory;
-        emit AddUserStory(_id, _userStory);
+        emit BuyProduct(_id, msg.sender, now, _userStory);
         return true;
     }
-    function confirmProduct(bytes32 _id)
-    public
-    returns (bool)
-    {
-        require(products[_id].owner == msg.sender);
-        require(products[_id].status == 1);
-        products[_id].status = 2;
-        emit ConfirmProduct(_id, true);
-        return true;
-    }
+    // function addUserStory(bytes32 _id, bytes _userStory)
+    // public
+    // returns (bool)
+    // {
+    //     require(products[_id].owner == msg.sender);
+    //     require(products[_id].status == 1);
+    //     products[_id].userStory = _userStory;
+    //     emit AddUserStory(_id, _userStory);
+    //     return true;
+    // }
+    // function confirmProduct(bytes32 _id)
+    // public
+    // returns (bool)
+    // {
+    //     require(products[_id].owner == msg.sender);
+    //     require(products[_id].status == 1);
+    //     products[_id].status = 2;
+    //     emit ConfirmProduct(_id, true);
+    //     return true;
+    // }
 }
